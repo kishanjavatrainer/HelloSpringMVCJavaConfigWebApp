9f5d32168f2cd027c788cea7e58abc218fbc698e
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-07-01 17:28:33 +0530

    HelloSpringMVCJavaConfigWebApp source code

---
+++ HelloSpringMVCWebApp/src/com/infotech/config/ServletInitializer.java
@@ -0,0 +1,24 @@
+package com.infotech.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class ServletInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext container) throws ServletException {
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.register(WebConfig.class);
+		context.setServletContext(container);
+		
+		Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(context));
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+		
+	}
+}
